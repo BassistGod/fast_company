5e28e3b1f25628443bd7bd2a7e8b0d18b97b4e73
Valentin <Vodopianova_19a8@mail.ru>
2023-04-20 13:58:32 +0700

    add Login

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  // const [email, setEmail] = useState("");
+  const [data, setData] = useState({ email: "", password: "" });
+  const handleChange = (target) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Пароль</label>
+        <input type="password" id="password" />
+      </div>
+    </form>
+  );
 };
 
 export default Login;
